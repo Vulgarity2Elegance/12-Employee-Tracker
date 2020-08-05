ba160ad159670bdba1c4bab662fd5cf55a9f88e4
Youzhi Hu <jyzhu0101@gmail.com>
2020-08-05 13:56:42 +1000

    Successfully connected to mysql localhost.

---
+++ server.js
@@ -0,0 +1,15 @@
+const { prompt } = require("inquirer");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Yozi-41313",
+    database: "employee_trackerDB",
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("It is connected to localhost 3306.");
+});
